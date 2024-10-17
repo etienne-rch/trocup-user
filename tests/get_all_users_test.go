7aab203b44f0c34179564c275aa10775956b1446
NSE-thik <anass.chemllal@e-thik.com>
2024-10-17 14:03:13 +0200

    add tests for get_all_users

--- tests/get_all_users_test.go
+++ tests/get_all_users_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -45,14 +46,40 @@ func TestGetUsers(t *testing.T) {
 			IsPremium:   true,
 		},
 	}
+
+	// Insérer les utilisateurs créés dans la base de données de test
 	for _, user := range users {
 		config.UserCollection.InsertOne(context.TODO(), user)
 	}
 
 	req := httptest.NewRequest("GET", "/users", nil)
-	resp, _ := app.Test(req, -1)
+	resp, err := app.Test(req, -1)
+	assert.NoError(t, err, "Failed to get response from server")
 
+	// Vérifier que la réponse HTTP renvoie un statut 200 OK
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	// Décoder le JSON de la réponse pour obtenir les utilisateurs
+	var response struct {
+		Users []models.User `json:"users"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	assert.NoError(t, err, "Failed to decode response body")
+
+	// Vérifier que le nombre d'utilisateurs retournés correspond à ceux que j'ai insérés dans la base de données
+	assert.Equal(t, len(users), len(response.Users), "Number of returned users does not match expected count")
+
+	// Comparer les champs des utilisateurs retournés avec ceux que j'ai insérés
+	for i, user := range users {
+		assert.Equal(t, user.Pseudo, response.Users[i].Pseudo)
+		assert.Equal(t, user.Name, response.Users[i].Name)
+		assert.Equal(t, user.Surname, response.Users[i].Surname)
+		assert.Equal(t, user.Email, response.Users[i].Email)
+		assert.Equal(t, user.Sexe, response.Users[i].Sexe)
+		assert.Equal(t, user.PhoneNumber, response.Users[i].PhoneNumber)
+		assert.Equal(t, user.IsPremium, response.Users[i].IsPremium)
+	}
 
 	// Nettoyage après chaque test
 	defer config.CleanUpTestDatabase("test_db")
