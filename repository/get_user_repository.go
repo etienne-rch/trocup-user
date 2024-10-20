8107e92cb00bacf7bee67ceff6fdd3d718ed1717
Lorie <root@DESKTOP-TCF3JUM>
2024-10-20 19:38:12 +0200

    add decode user id in repository

--- repository/get_user_repository.go
+++ repository/get_user_repository.go
@@ -2,18 +2,30 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"trocup-user/config"
 	"trocup-user/models"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetUserByID(id string) (*models.User, error) {
 	var user models.User
 
-	// Recherche dans MongoDB avec le Clerk ID (qui est un string)
-	err := config.UserCollection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&user)
+	// Conversion de l'ID string en ObjectID
+	objectId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
+		return nil, errors.New("invalid ID format")
+	}
+
+	// Recherche dans MongoDB avec le Clerk ID (qui est un string)
+	err = config.UserCollection.FindOne(context.TODO(), bson.M{"_id": objectId}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.New("user not found")
+		}
 		return nil, err
 	}
 	return &user, nil
