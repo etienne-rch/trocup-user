f8cbdb215e5d7b3c91d1f5a3c6dd91995c80076e
Erwan Cherel <erwancherel.pro@gmail.com>
2024-11-14 08:59:02 +0100

    Update models

--- models/user.go
+++ models/user.go
@@ -8,13 +8,13 @@ import (
 
 type Address struct {
 	Label     string    `json:"label,omitempty" bson:"label,omitempty"`
-	Street     string    `json:"street,omitempty" bson:"street,omitempty"`
-	City       string    `json:"city,omitempty" bson:"city,omitempty"`
-	Postcode   string       `json:"postcode,omitempty" bson:"postcode,omitempty"`
-	Citycode   string       `json:"citycode,omitempty" bson:"citycode,omitempty"`
-	Floor      int       `json:"floor,omitempty" bson:"floor,omitempty"`
-	Extra      string    `json:"extra,omitempty" bson:"extra,omitempty"`
-	GeoPoints  GeoPoints `json:"geopoints,omitempty" bson:"geopoints,omitempty"`
+	Street    string    `json:"street,omitempty" bson:"street,omitempty"`
+	City      string    `json:"city,omitempty" bson:"city,omitempty"`
+	Postcode  string    `json:"postcode,omitempty" bson:"postcode,omitempty"`
+	Citycode  string    `json:"citycode,omitempty" bson:"citycode,omitempty"`
+	Floor     int       `json:"floor,omitempty" bson:"floor,omitempty"`
+	Extra     string    `json:"extra,omitempty" bson:"extra,omitempty"`
+	GeoPoints GeoPoints `json:"geopoints,omitempty" bson:"geopoints,omitempty"`
 }
 
 type GeoPoints struct {
@@ -33,23 +33,24 @@ type BankInfo struct {
 }
 
 type User struct {
-	ID               string             `json:"id,omitempty" bson:"_id,omitempty"` // Clerk's user ID
-	Version          int                `json:"version,omitempty" bson:"version,omitempty"`
-	Pseudo           string             `json:"pseudo" bson:"pseudo" validate:"required"`
-	Name             string             `json:"name,omitempty" bson:"name,omitempty" validate:"required"`
-	Surname          string             `json:"surname,omitempty" bson:"surname,omitempty" validate:"required"`
-	Address          []Address            `json:"address,omitempty" bson:"address,omitempty"`
-	Email            string             `json:"email" bson:"email,omitempty" validate:"required,email"`
-	Sexe             string             `json:"sexe,omitempty" bson:"sexe,omitempty" validate:"oneof=M F"`
-	PhoneNumber      string             `json:"phoneNumber,omitempty" bson:"phoneNumber,omitempty" validate:"omitempty,e164"`
-	ActivityStatus   ActivityStatus     `json:"activityStatus,omitempty" bson:"activityStatus,omitempty"`
-	BirthDate        time.Time          `json:"birthDate,omitempty" bson:"birthDate,omitempty"`
-	BankInfo         *BankInfo          `json:"bankInfo,omitempty" bson:"bankInfo,omitempty"`
-	AvatarUrl        string             `json:"avatarUrl,omitempty" bson:"avatarUrl,omitempty"`
-	IsPremium        bool               `json:"isPremium,omitempty" bson:"isPremium,omitempty"`
-	FavoriteArticles []string           `json:"favoriteArticles,omitempty" bson:"favoriteArticles,omitempty"`
-	Credit           float64            `json:"credit,omitempty" bson:"credit,omitempty"`
-	Balance          float64            `json:"balance,omitempty" bson:"balance,omitempty"`
-	Comments         []string           `json:"comments,omitempty" bson:"comments,omitempty"`
-	Articles         []string           `json:"articles,omitempty" bson:"articles,omitempty"`
+	ID               string         `json:"id,omitempty" bson:"_id,omitempty"` // Clerk's user ID
+	Version          int            `json:"version,omitempty" bson:"version,omitempty"`
+	Pseudo           string         `json:"pseudo" bson:"pseudo" validate:"required"`
+	Name             string         `json:"name,omitempty" bson:"name,omitempty" validate:"required"`
+	Surname          string         `json:"surname,omitempty" bson:"surname,omitempty" validate:"required"`
+	Address          []Address      `json:"address,omitempty" bson:"address,omitempty"`
+	Email            string         `json:"email" bson:"email,omitempty" validate:"required,email"`
+	Sexe             string         `json:"sexe,omitempty" bson:"sexe,omitempty" validate:"oneof=M F"`
+	PhoneNumber      string         `json:"phoneNumber,omitempty" bson:"phoneNumber,omitempty" validate:"omitempty,e164"`
+	ActivityStatus   ActivityStatus `json:"activityStatus,omitempty" bson:"activityStatus,omitempty"`
+	BirthDate        time.Time      `json:"birthDate,omitempty" bson:"birthDate,omitempty"`
+	BankInfo         *BankInfo      `json:"bankInfo,omitempty" bson:"bankInfo,omitempty"`
+	AvatarUrl        string         `json:"avatarUrl,omitempty" bson:"avatarUrl,omitempty"`
+	IsPremium        bool           `json:"isPremium,omitempty" bson:"isPremium,omitempty"`
+	FavoriteArticles []string       `json:"favoriteArticles,omitempty" bson:"favoriteArticles,omitempty"`
+	Credit           float64        `json:"credit,omitempty" bson:"credit,omitempty"`
+	Balance          float64        `json:"balance,omitempty" bson:"balance,omitempty"`
+	Comments         []string       `json:"comments,omitempty" bson:"comments,omitempty"`
+	Articles         []string       `json:"articles,omitempty" bson:"articles,omitempty"`
+	Debit            []string       `json:"debit,omitempty" bson:"debit,omitempty"`
 }
